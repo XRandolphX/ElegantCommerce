dabf963a15de826e4bbac089624b3c506020d632
Randolph Fabrizio Ramirez Palacios <randolphfrp@gmail.com>
2025-03-14 15:56:59 -0500

    adding paypal dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,6 +153,11 @@ dependencies {
     // Activity Compose
     implementation("androidx.activity:activity-compose:1.4.0")
 
+    //Paypal Dependencies
+    implementation("com.github.amitshekhariitbhu.Fast-Android-Networking:android-networking:1.0.4")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.google.code.gson:gson:2.12.1")
+    implementation ("com.paypal.android:paypal-web-payments:1.7.1")
 }
 
 // Allow references to generated code
