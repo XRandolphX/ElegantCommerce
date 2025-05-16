e77bb75c0c2f5d64e2294b8ba8f4ce0d9854f840
XRandolphX <randolphfrp@gmail.com>
2025-05-16 13:47:11 -0500

    Update deep links for payment activities and enhance UI messages for failure and pending states

--- app/src/main/java/com/xrandolphx/elegantcommerce/activity/FailureActivity.kt
+++ app/src/main/java/com/xrandolphx/elegantcommerce/activity/FailureActivity.kt
@@ -1,18 +1,13 @@
 package com.xrandolphx.elegantcommerce.activity
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.xrandolphx.elegantcommerce.R
 
 class FailureActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_failure)
-
-        val tvMessage = findViewById<TextView>(R.id.tvMessage)
-        tvMessage.text = "El pago falló. Intenta nuevamente o contacta soporte."
     }
 }
--- app/src/main/java/com/xrandolphx/elegantcommerce/activity/PendingActivity.kt
+++ app/src/main/java/com/xrandolphx/elegantcommerce/activity/PendingActivity.kt
@@ -1,18 +1,12 @@
 package com.xrandolphx.elegantcommerce.activity
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.xrandolphx.elegantcommerce.R
 
 class PendingActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pending)
-
-        val tvMessage = findViewById<TextView>(R.id.tvMessage)
-        tvMessage.text = "Tu pago está pendiente. Recibirás una notificación cuando se confirme."
     }
 }
